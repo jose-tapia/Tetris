3b3d0c97c3a7579b56846f3cdb91e4eefc409344
José Tapia <jose.tapiaav@gmail.com>
2017-12-16 20:24:44 -0600

    Delete tests.cpp

--- tests.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  tests.cpp
-//  Tetris
-//
-//  Created by Jose Manuel Tapia Avitia on 13/12/17.
-//  Copyright (c) 2017 Jose Manuel Tapia Avitia. All rights reserved.
-//
-
-#include "tests.h"
